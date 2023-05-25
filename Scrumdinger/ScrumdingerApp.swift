998a8f5580914316cb4a3e8f262e14dd56773b3f
Kelvin C. Ricafort <kelvs.r@outlook.com>
2023-05-25 20:48:27 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Kelvin on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
