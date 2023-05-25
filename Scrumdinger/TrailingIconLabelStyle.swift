ea739dc87cc92f4c5c4fd051961ee11a71996c00
Kelvin C. Ricafort <kelvs.r@outlook.com>
2023-05-25 22:45:28 +0800

    Displaying data in a list

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Kelvin on 5/25/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
